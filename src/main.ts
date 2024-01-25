8912d65375f349dab08882f68ccb4e0ee90dc341
AndrewwBC <andrew-hot2011@hotmail.com>
2024-01-25 18:46:20 -0300

    feat: repositories and global modules

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { PrismaService } from './shared/database/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
