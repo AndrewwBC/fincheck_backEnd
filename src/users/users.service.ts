8fb2a6da1aa789b19b78f58a283a0e45bead2249
Andrew <andrew-hot2011@hotmail.com>
2024-01-25 09:34:22 -0300

    feat: pipes

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,10 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/database/prisma.service';
+import { hash } from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
+  constructor(private readonly prismaService: PrismaService) {}
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    const { name, email, password } = createUserDto;
+
+    const emailTaken = await this.prismaService.user.findUnique({
+      where: { email },
+    });
+
+    if (emailTaken) throw new ConflictException('Email already in use.');
+
+    const user = await this.prismaService.user.create({
+      data: { name, email, password },
+    });
+
+    return createUserDto;
   }
 }
